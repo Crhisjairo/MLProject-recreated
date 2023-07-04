4b69ef330307b819cab6f7872140a2c60cd8062f
Christian Castaneda <crhisjairo@hotmail.com>
2023-07-04 10:37:47 -0400

    Tiled project added
    - SuperTiled2Unity added

---
+++ Assets/Assets/Maps/MainVille/Lamp_violet_single.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Lamp_violet_single" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../Sprites/Map/Decorations/Listas/Lamp_violet_single.png" width="64" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
