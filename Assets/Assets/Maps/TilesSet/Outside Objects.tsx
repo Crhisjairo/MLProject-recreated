68a1a73349e7113f6c82698d93711d96efaec9fa
Christian Castaneda <crhisjairo@hotmail.com>
2023-09-09 01:29:54 -0400

    Tutorial creation

--- Assets/Assets/Maps/TilesSet/Outside Objects.tsx
+++ Assets/Assets/Maps/TilesSet/Outside Objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Outside Objects" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+<tileset version="1.10" tiledversion="1.10.1" name="Outside Objects" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
  <image source="../Sprites/Map/Objects/Outside Objects.png" width="1024" height="1024"/>
 </tileset>
